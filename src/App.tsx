e161905de59536c3672fee0267aae127ed54987d
samaroec <samar.saafi@openeyesconsulting.com>
2023-11-29 14:33:28 +0100

    First Commit

---
+++ src/App.tsx
@@ -0,0 +1,57 @@
+
+
+import { useRef, useState } from "react";
+import "./App.css";
+
+function App() {
+  const [todos, setTodos] = useState<any[]>([]);
+  const inputRef = useRef<HTMLInputElement>(null);
+  const handleAddTodo = () => {
+    const text = inputRef.current?.value;
+    const newItem = { completed: false, text };
+    setTodos([...todos, newItem]);
+    inputRef.current!.value = "";
+  };
+
+  const handleItemDone = (index: any) => {
+    const newTodos = [...todos];
+    newTodos[index].completed = !newTodos[index].completed;
+    setTodos(newTodos);
+  };
+
+  const handleDeleteItem = (index: any) => {
+    const newTodos = [...todos];
+    newTodos.splice(index, 1);
+    setTodos(newTodos);
+  };
+
+  return (
+    <div className="App">
+      <h2>To Do List</h2>
+      <div className="to-do-container">
+        <ul>
+          {todos.map(({ text, completed }, index) => {
+            return (
+              <div className="item">
+                <li
+                  className={completed ? "done" : ""}
+                  key={index}
+                  onClick={() => handleItemDone(index)}
+                >
+                  {text}
+                </li>
+                <span onClick={() => handleDeleteItem(index)} className="trash">
+                  ❌
+                </span>
+              </div>
+            );
+          })}
+        </ul>
+        <input ref={inputRef} placeholder="Enter item..." />
+        <button onClick={handleAddTodo}>Add</button>
+      </div>
+    </div>
+  );
+}
+
+export default App;
